a7dbac0127067208543e2b3331437dd9c72844e9
Brian Dineen <brian.dineen@acuitybrands.com>
2016-10-12 08:42:49 -0400

    Initial Commit

---
+++ Mathomatic/Mathomatic.h
@@ -0,0 +1,19 @@
+//
+//  Mathomatic.h
+//  Mathomatic
+//
+//  Created by Brian Dineen on 10/12/16.
+//  Copyright © 2016 AcuityBrands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mathomatic.
+FOUNDATION_EXPORT double MathomaticVersionNumber;
+
+//! Project version string for Mathomatic.
+FOUNDATION_EXPORT const unsigned char MathomaticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mathomatic/PublicHeader.h>
+
+
